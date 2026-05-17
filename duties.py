4a4088bc19d3364500d65803fec5b45690dd01a4
Cristian Cardellino <crscardellino@gmail.com>
2026-05-17 10:37:20 -0300

    Updated duties with new schema

--- duties.py
+++ duties.py
@@ -13,32 +13,39 @@ SETTINGS.update(DEFAULT_CONFIG)
 LOCAL_SETTINGS = get_settings_from_file("pelicanconf.py")
 SETTINGS.update(LOCAL_SETTINGS)
 
-OUTPUT_DIRECTORY = SETTINGS.get("OUTPUT_DIRECTORY", "_site")
+OUTPUT_DIRECTORY = SETTINGS.get("OUTPUT_DIRECTORY", "output")
 
 run_pelican = tools.lazy(pelican_main, name="pelican.main")
 
 
-@duty
+@duty(capture=False)
+def build(ctx: Context) -> None:
+    """Run the site linting, and builds it"""
+    clean(ctx)
+    ctx.run(["ruff", "format", "--check", "."])
+    ctx.run(["ruff", "check", "."])
+    ctx.run(run_pelican(["-s", "pelicanconf.py", "-o", OUTPUT_DIRECTORY]))
+
+
+@duty(capture=False)
 def clean(ctx: Context) -> None:
     """Clean the _site directory."""
-    site_dir = Path("output")
+    site_dir = Path(OUTPUT_DIRECTORY)
     if site_dir.is_dir():
         shutil.rmtree(site_dir)
 
 
 @duty(capture=False)
-def devserver(ctx: Context, port: int = 4000) -> None:
+def devserver(ctx: Context) -> None:
     """Start dev server with autoreload"""
-    ctx.run(
-        run_pelican(["-s", "pelicanconf.py", "-o", OUTPUT_DIRECTORY, "-r", "-l", "-p", f"{port}"])
-    )
+    ctx.run(run_pelican(["-s", "pelicanconf.py", "-o", OUTPUT_DIRECTORY, "-r", "-l", "-p", "4000"]))
 
 
 @duty(capture=False)
 def lint(ctx: Context) -> None:
-    """Run linting commands for checking"""
-    ctx.run(["ruff", "format", "--check", "."])
-    ctx.run(["ruff", "check", "."])
+    """Run formating and linting for fixing"""
+    ctx.run(["ruff", "format", "."])
+    ctx.run(["ruff", "check", "--fix", "."])
 
 
 @duty
